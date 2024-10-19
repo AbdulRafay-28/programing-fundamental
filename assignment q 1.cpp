d7eeb7affd775a17f189d066008836884483cbbf
AbdulRafay-28 <rafaypathan28@gmail.com>
2024-10-18 23:05:42 -0700

    Add files via upload

---
+++ assignment q 1.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main(); 
+int a=2; b=5;
+{
+ int a,b 'sum';
+ printf ("enter two integer");
+ scanf("%d %d" ,&a, &b);
+ sum=a+b;
+ printf("the sum is: %d\n",sum);
+ getch();
+  }                        
