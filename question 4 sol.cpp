538d373f5dacca1e22b4ab4c16098ea5292fe635
ABDUL RAFY <rafaypathan28@gmail.com>
2024-10-18 23:56:03 -0700

    Add files via upload

---
+++ question 4 sol.cpp
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	int celsius=8,fahrenheit;
+	fahrenheit=(celsius*1.8)+32;
+	printf("%f celsius=%f fahrenheit",celsius,fahrenheit);
+	getch();
+	
+}
